42d1f2de002f6c99f7cf61f32a9adde1098e37f0
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2023-12-20 18:10:45 +0200

    Add files via upload

---
+++ Cascade_cmpr.v
@@ -0,0 +1,10 @@
+module CscdeCmprtr(CAS[0:2])
+inout  CAS[0:2];
+
+/*
+1. 8259A outputs are master while the inputs are
+slaves 
+2.8259A (Master) sends the ID on CAS
+3. Slave sends its routine 
+*/
+endmodule
