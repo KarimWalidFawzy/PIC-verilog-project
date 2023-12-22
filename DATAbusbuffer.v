99dac195cfeaaa5f437920f7a6538d74a35acd8a
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2023-12-22 16:55:06 +0200

    DATAbusbuffer.v

--- DATAbusbuffer.v
+++ DATAbusbuffer.v
@@ -1,10 +1,26 @@
 module buffer(D[0:7],inta,PCadr[0:7],counter[0:1]);
-inout D[0:7];
+inout D[0:7];/*
+D may contain: 
+-Control info to/from the Control logic
+-Status (read or write)
+-Interrupt vector bus info
+*/
+reg bfr[0:7];
+reg Mode;
 input inta;
-parameter CALLCODEMCS=8'b11001101;
 inout PCadr[0:7];
 always @(negedge inta)
+/*if  MCS */
+//for first pulse
+if(counter=2b'10)
+bfr[0:7]=D[0:7];
+PCadr[0:7]=bfr[0:7];
+if(Mode)begin
+    isr<=isr&~Mode;
+end
 
+//for second and 3rd pulse
+//PCadr=Subroutine
 
 
 
