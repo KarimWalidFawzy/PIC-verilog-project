05a665122439435f288b71e127f657305dd1ec9b
MohamedAlii59 <126531693+MohamedAlii59@users.noreply.github.com>
2023-12-23 21:43:30 +0200

    Update Prrty_res.v

--- Prrty_res.v
+++ Prrty_res.v
@@ -1,3 +1,15 @@
-module PRes();
+module Priority_resolver(
+  input fn,     //fully nested mode
+  input ar,      //autmatic_rotation
+  input eoi,    //end of interrupt
+  input reg[7:0] irr,  //interrupt request register
+  input reg[7:0] imr, // interrupt mask register
+  output rep[7:0] isr, // in service register
+);
+  reg [7:0] irr_masked;
+  irr_masked = irr & ~ imr;
+  always @ begin
+    if(fn)
+    
 
 endmodule
