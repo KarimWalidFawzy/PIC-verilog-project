ea89f8044f55ecd4fe97a2a35fe2d38f7d9192ed
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2023-12-23 18:38:02 +0200

    Cascade_cmpr.v

--- Cascade_cmpr.v
+++ Cascade_cmpr.v
@@ -1,6 +1,26 @@
-module CscdeCmprtr(CAS[0:2])
+module CscdeCmprtr(CAS[0:2],SPENn,M,CLsig,Y[0:2])
 inout  CAS[0:2];
+inout Y[0:2];
+if(M)begin
+    case(Y[0:2])
+    3'b111:/* contains the */ CAS[0:2]=3'b111;
+    3'b110:;
+    5:;
+    4:;
+    3:;
+    2:;
+    1:;
+    default CAS=3'b000;
+    endcase
+end
+if(~M)begin
+    if(CAS[0:2]==Y[0:2])begin
+        CLsig=1;
+    end
+end
 
+
+reg count[];
 /*
 1. 8259A outputs are master while the inputs are
 slaves 
