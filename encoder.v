f1246a342915743ed16d9f911627eac2ecdb6544
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2023-12-25 22:21:58 +0200

    Update encoder.v

--- encoder.v
+++ encoder.v
@@ -1,6 +1,14 @@
 module encode(
-    input twotothen[0:7],
-    output n[0:2]);
-
+    input i[0:7],
+    output y[0:2]);
+    if(i[7]==1) y=3'b111;
+        else if(i[6]==1) y=3'b110;
+        else if(i[5]==1) y=3'b101;
+        else if(i[4]==1) y=3'b100;
+        else if(i[3]==1) y=3'b011;
+        else if(i[2]==1) y=3'b010;
+        else if(i[1]==1) y=3'b001;
+        else
+        y=3'b000;
 
 endmodule
