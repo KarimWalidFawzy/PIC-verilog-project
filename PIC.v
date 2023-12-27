ba21249c4a670f1fcafd987eb050ec744f87bb3e
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2023-12-27 14:14:48 +0200

    Update PIC.v

--- PIC.v
+++ PIC.v
@@ -1,35 +1,35 @@
 module Intel8257A(
-    inout D[0:7],
+    inout [0:7] D,
     input NRD,
     input NWR,
     input NCS,
-    inout CAS[0:2],
+    inout [0:2] CAS,
     input A0,
     input NINTA,
-    input IR[0:7],
+    input [0:7] IR,
     output INT,
     input NSP_EN);
-reg PCadr[0:7];
+wire [0:7] PCadr;
 wire en;
 wire ino;
 buffer(D,PCadr,en,ino);
 wire wrflg;
 wire buff;
 wire rdflg;
-wire R[0:7];
-wire cadr[0:2];
+wire [0:7] R;
+wire [0:2] cadr;
 wire b0;
-wire Y[0:2];
+wire [0:2] Y;
 R_WCtrllgc(NRD,NWR,A0,NCS,wrflg,rdflg,cadr[0:2],R[0:7],b0);
 wire S;
 wire CLsig;
-wire Mask[0:7];
-reg irr[0:7];
-reg isr[0:7];
+reg [0:7] Mask;
+reg [0:7] irr;
+reg [0:7] isr;
 wire isprior;
 wire LTIM;
 wire reset;
-reg imr[0:7];
+reg [0:7] imr;
 wire ar;
 wire eoi;
 CscdeCmprtr(CAS[0:2],NSP_EN,CLsig,Y[0:2],buff,S);
@@ -37,6 +37,5 @@ ISR(irr,isr,isprior);
 ctrllgc(INT,NINTA,PCadr,ino,en,A0,wrflg,rdflg,R[0:7],cadr[0:2],Y[0:2],S,buff,CLsig,Mask,isr,irr,LTIM,isprior,eoi,ar,reset);
 IRR(reset,LTIM,IR[0],IR[1],IR[2],IR[3],IR[4],IR[5],IR[6],IR[7],irr[0:7]);
 IMR(Mask[0:7],imr[0:7]);
-reg fn=~ar;
-Priority_resolver(fn,ar,eoi,irr[0:7],imr[0:7],isr[0:7],isprior,NINTA);
+Priority_resolver(~ar,ar,eoi,irr[0:7],imr[0:7],isr[0:7],isprior,NINTA);
 endmodule
