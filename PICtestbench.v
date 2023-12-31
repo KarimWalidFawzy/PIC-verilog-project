54afa89a95951b46f2875d8af7bdc52b42da73e8
MohamedAlii59 <126531693+MohamedAlii59@users.noreply.github.com>
2023-12-31 22:43:06 +0200

    Update PICtestbench.v

---
+++ PICtestbench.v
@@ -0,0 +1,51 @@
+`timescale 1ns/1ps // Define timescale
+
+module testbench();
+
+  // Declare signals
+  reg [7:0] D;
+  reg NRD, NWR, NCS, A0, NINTA, NSP_EN, reset;
+  reg [7:0] IR;
+  wire INT, en, ino, NSP_EN_wire, buff, rdflg, S, CLsig, LTIM, eoi, ar, isprior;
+  wire [2:0] CAS;
+  wire [0:7] PCadr, R, Mask, irr, isr, imr;
+  wire [0:2] cadr, Y;
+  
+  // Instantiate the Intel8257A module to be tested
+  Intel8257A dut (
+    .D(D),
+    .NRD(NRD),
+    .NWR(NWR),
+    .NCS(NCS),
+    .CAS(CAS),
+    .A0(A0),
+    .NINTA(NINTA),
+    .IR(IR),
+    .INT(INT),
+    .NSP_EN(NSP_EN)
+  );
+
+  // Instantiate other instantiated modules here (buffer, R_WCtrllgc, etc.)
+
+  // Stimulus generation
+  initial begin
+    // Initialize inputs
+    D = 8'b00000000;
+    NRD = 0;
+    NWR = 0;
+    NCS = 0;
+    A0 = 0;
+    NINTA = 0;
+    NSP_EN = 0;
+    reset = 0;
+    IR = 8'b00000000; // Simulate IR values as needed
+
+    #10; // Wait for stabilization
+
+    // Apply some test stimuli
+    // Change input values as needed and apply test scenarios
+
+    $finish;
+  end
+
+endmodule
